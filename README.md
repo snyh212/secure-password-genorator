d9083a6ea566983de6f30b4dd46fce1a0a12be51
Shmuel Hoffman <snyh212@gmail.com>
2022-06-24 23:53:04 -0400

    last commit before sabbath

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # secure-password-genorator
+
+Deployable Link:
+
+## Descriptiom
+
+The purpose of "secure-password-genorator" is to allow a user to have a secure password genorated for them based on a couple specific criteria. The user is prompted whether to use uppercase letters, lowercase letters, numbers and special charachters.
+
+## Technologies used
+
+Project done using HTML, CSS, JavaScript, Github, GitLab on VS code.
+
+## Contact
+
+[E-mail: Snyh121@gmail.com](mailto:snyh121@gmail.com")  
+[GitHub: snyh212](https://r.search.yahoo.com/_ylt=AwrJ6yegl7JipfcAzB5XNyoA;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1655900193/RO=10/RU=https%3a%2f%2fgithub.com%2fsnyh212/RK=2/RS=jAFa0VbZnIusPrwj.ZmIx9gZ3AA-)  
+[LinkdIn: Shmuel-Hoffman](https://www.linkedin.com/in/shmuel-hoffman-254b0223b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BS2rg0PtBTLeG2szT2ZbGmg%3D%3D)
--- script.js
+++ script.js
@@ -8,30 +8,38 @@ var sCharacter = "$_&";
 // Write password to the #password input
 function generatePassword(){
   /* prompting user for password criteria */
+  var options = "";
   var passwordLength = prompt("How many characters would you like your password to contain?");
   if (passwordLength < 8) { /* password minimum */
     alert("Password must be at least 8 charachters!");
+  /* redo generatePassword function */
     passwordLength = prompt("How many characters would you like your password to contain?");
-
   } else if (passwordLength > 128){ /* password maximum */
   alert("Password must be less than 128 charachters!");
   passwordLength = prompt("How many characters would you like your password to contain?");
-  } else { /* allows useer to chose upper/lowercase */
+  } else { /* allows user to chose upper/lowercase */
     var upCaseUser = confirm("Would you like to use uppercase letters?");
     if (upCaseUser == false){
         var loCaseUser = confirm("Would you like to use lowercase letters?");
+        if (loCaseUser) options += lCase;
     }else {
+      options += uCase;
       loCaseUser = confirm("Would you like to use lowercase letters?");
     }
   } if (loCaseUser == false && upCaseUser == false){ /* alerts user to use letters */
     alert("Password must contain letters!")
   } else {
-    var numUser = confirm("Would you like to use numbers?")/*  */
+    var numUser = confirm("Would you like to use numbers?")/* allows user to use numbers */
   } if (numUser == true || numUser == false){
-    var sCharUser = confirm("Would you like to use special charachters?");
+    var sCharUser = confirm("Would you like to use special characters?");
+    if (numUser) options += number;
+  }
+  var res = ""
+  for(i = 0; i < passwordLength; i++){
+    res += options[Math.random(options)]
   }
 
-
+  alert (res);
 }
 
 
